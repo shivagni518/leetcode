ca2cc2927354b3f7d28b23638eea34fe02ada1fd
Shivagni A R <shivagnigowda513@gmail.com>
2025-08-19 15:05:52 +0530

    Time: 5 ms (65.82%) | Memory: 44.3 MB (25.50%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+    int temp=x;
+    int rev=0;
+    while(temp!=0){
+        int digit=0;
+        digit=temp%10;
+        rev=rev*10+digit;
+        temp/=10;
+    }if(x==rev){
+        return true;
+    }else{
+        return false;
+    }  
+    }
+}
