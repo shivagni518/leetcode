8c80c868cd4372d99d34c08b86b7851b80e46a2d
Shivagni A R <shivagnigowda513@gmail.com>
2025-08-19 21:03:18 +0530

    Time: 8 ms (81.96%) | Memory: 44.4 MB (18.77%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -4,8 +4,7 @@ class Solution {
             return true;
         }else if(n<1||n%3!=0){
             return false;
-        }else{
-            return isPowerOfThree(n/3);
         }
+            return isPowerOfThree(n/3);
     }
 }
