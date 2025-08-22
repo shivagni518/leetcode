ae96a165a4c9dbc46831d6bf58d513a9bec23591
Shivagni A R <shivagnigowda513@gmail.com>
2025-08-22 22:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.16%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int red=0,white=0,blue=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                red++;
+            }else if(nums[i]==1){
+                white++;
+            }
+            else{
+            blue++;
+            }
+        }
+        int index=0;
+        for(int i=0;i<red;i++){
+            nums[index++]=0;
+        }
+        for(int i=0;i<white;i++){
+            nums[index++]=1;
+        }
+        for(int i=0;i<blue;i++){
+            nums[index++]=2;
+        }  
+    }
+}
