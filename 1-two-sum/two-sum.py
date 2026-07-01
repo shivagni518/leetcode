b422dbad8065441ff95ec8c9758478581b06052a
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-01 16:08:37 +0530

    Time: 1734 ms (20.07%) | Memory: 19.9 MB (72.58%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,5 +3,5 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
-                    return[i,j]
+                    return i,j
         
