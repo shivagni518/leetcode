033762fe392539123ec5f7153f137f72f7415ad4
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-03 07:14:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.94%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        # max=0
+        # for customer in accounts:
+        #     wealth=0
+        #     for money in customer:
+        #         wealth=wealth+money
+        #     if wealth>max:
+        #         max=wealth
+        # return max        
+
+        return max([sum(customer) for customer in accounts])
+        
