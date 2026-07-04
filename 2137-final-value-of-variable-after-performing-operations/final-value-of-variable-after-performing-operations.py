9e75da01eb09dc98b53118af366e02f39a3b3863
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-04 13:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.96%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -4,8 +4,10 @@ class Solution:
         for i in range(len(operations)):
             if operations[i]=="++X" or operations[i]=="X++":
                 x=x+1
-            elif operations[i]=="--X" or operations[i]=="X--":
-                x=x-1
+            # elif operations[i]=="--X" or operations[i]=="X--":
+            #     x=x-1
+            else:
+                x=x-1    
         return x            
 
         
