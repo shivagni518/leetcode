fc347c6fe84063067f985d021749ed26356758b2
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-15 22:00:27 +0530

    Time: 8 ms (49.47%) | Memory: 19.7 MB (52.80%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean=""
+        for i in s:
+            if i.isalnum():
+                clean+=i.lower()
+        return clean==clean[::-1]        
+            
+        
