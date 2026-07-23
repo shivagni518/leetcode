d9a546759310dfa376ace0be99775e8360796c28
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-23 09:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (75.02%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,15 +5,15 @@ class Solution:
         #         return i
         # return -1    
 
-        left = 0
-        right = len(nums)-1
-        while left <= right:
-            mid = (left+right)//2
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (start+end)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid + 1
+                start = mid + 1
             else:
-                right = mid - 1
+                end = mid - 1
         return -1                
         
