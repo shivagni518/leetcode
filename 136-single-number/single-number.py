177c0e813667545359787b89525413bf144e08fc
Shivagni A R <shivagnigowda513@gmail.com>
2026-07-25 19:48:53 +0530

    Time: 11 ms (16.82%) | Memory: 21.7 MB (10.26%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            count=0
-            for j in nums:
-                if i == j:
-                    count+=1
-            if count == 1:
-                return i        
+        # for i in nums:
+        #     count=0
+        #     for j in nums:
+        #         if i == j:
+        #             count+=1
+        #     if count == 1:
+        #         return i   
+
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        for num in freq:
+            if freq[num] == 1:
+                return num     
         
