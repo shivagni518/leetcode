90576108005d8fa10bb26ffda6a4a4747d27acb9
Shivagni A R <shivagnigowda513@gmail.com>
2026-08-02 16:02:41 +0530

    Time: 23 ms (54.21%) | Memory: 19.6 MB (33.20%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,15 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # freq1={}
-        # freq2={}
-        # for i in ransomNote:
-        #     freq1[i] = freq1.get(i,0)+1
-        # for i in magazine:
-        #     freq2[i] = freq2.get(i,0)+1
+        freq1={}
+        freq2={}
+        for i in ransomNote:
+            freq1[i] = freq1.get(i,0)+1
+        for i in magazine:
+            freq2[i] = freq2.get(i,0)+1
 
-        # for i in freq1:
-        #     if freq1[i] > freq2.get(i,0):
-        #         return False
-        # return True                  
+        for i in freq1:
+            if freq1[i] > freq2.get(i,0):
+                return False
+        return True                  
         
-        return not (Counter(ransomNote) - Counter(magazine))
+        # return not (Counter(ransomNote) - Counter(magazine))
