d004656ea1825d142400467f9b453601e391737a
Shivagni A R <shivagnigowda513@gmail.com>
2026-08-03 11:45:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -7,5 +7,11 @@ class Solution:
                 if i == len(strs[j]) or strs[j][i] != ch:
                     return ans
             ans += ch
-        return ans            
+        return ans  
+
+        for i in range(len(strs[0])):
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return strs[0][:2]
+            return ""                 
                         
