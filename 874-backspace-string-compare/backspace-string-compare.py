4320e934a4f1ef4dca7775eee7de9a4bdc77b473
Shivagni A R <shivagnigowda513@gmail.com>
2026-08-06 22:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.08%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -6,13 +6,11 @@ class Solution:
             if ch != "#":
                 s1 += ch
             else:
-                # if s1:
                 s1 = s1[:-1]
         for ch in t:
             if ch != "#":
                 t1 += ch
             else:
-                # if t1:
                 t1 = t1[:-1]
         return s1 == t1
                          
