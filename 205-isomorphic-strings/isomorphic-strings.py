674007af130ef92f167e7bb17d1253462d39c318
Shivagni A R <shivagnigowda513@gmail.com>
2026-08-12 16:31:45 +0530

    Time: 4 ms (60.46%) | Memory: 19.2 MB (82.47%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        freq={}
+        back={}
+        for i in range(len(s)):
+            if s[i] in freq:
+                if freq[s[i]] != t[i]:
+                    return False
+            else:
+                freq[s[i]] = t[i]
+            if t[i] in back:
+                if back[t[i]] != s[i]:
+                    return False
+            else:
+                back[t[i]] = s[i]           
+        return True                    
+
+
+        
